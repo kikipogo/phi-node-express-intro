5aa70da65d35230d63e6c259860295ca0a90f2a0
Christine Pogatchnik <christinepogatchnik@Christines-MacBook-Pro.local>
2017-02-21 21:43:06 -0600

    Express Lecture with Notes

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+// TO GET UP AND GOING
 // cd to directory
 // npm init will put in json file
 // npm install express to get express --save
-// add .gitignore 
-var express = require('express');
+// add .gitignore file // in .gitignore put node_modules
+// in package.json after "test" add "start": "node app.js" to use npm start
+// make ajax request in client.js
+// npm install --save body-parser
+
+// var express = require('express'); // returns express from node_modules
+// var app = express(); // making an app that is an object
+// app.use(express.static('public')); //run this everytime a request comes in look for static file in public folder
+// app.listen(3000); // on local host 3000
+
+var express = require('express'); // returns express from node_modules
+var app = express(); // making 'app' which is an object
+var bodyParser = require('body-parser'); // library to send data from client to server
+
+app.use(express.static('public')); //run this everytime a request comes in look for static file in public folder
+
+app.use(bodyParser.urlencoded({extended: true})); //gets ahold of data we just created inputs
+
+var songList = [
+  {
+    title: 'We did not start the Phire',
+    artist: 'Billy Joel'
+  },
+  {
+    title: 'Ring of Phire',
+    artist: 'Johnny Cash'
+  }
+];
+
+app.get('/songs', function(req, res){
+  res.send(songList);
+});
+
+app.post('/newSong', function(req, res){
+  var newSong = req.body; // defined in client.js -- function getAllSongs()
+  if(newSong.artist !== 'Justin Bieber'){ // if artist IS NOT Justin Bieber run as normal
+    songList.push(newSong); // adding newSong to newSongObject
+    console.log(songList);
+    res.sendStatus(200); //cool
+  } else {
+    res.sendStatus(500);//500 is error if artist is Justin Bieber
+  }
+});
+
+app.listen(3000); // always listening on localhost: 3000
--- public/client.js
+++ public/client.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
   console.log('jquery was correctly sourced!');
-})
+  getAllSongs(); // remember that we must call function here > re-examine this! So it is posted before new entry?
+  function getAllSongs(){
+    $.ajax({
+      type: 'GET', // retrive resource of url
+      url: '/songs', //IN APP.JS > app.get('/songs', function(req, res){res.send(songList);})
+      success: function(response){
+        console.log('response', response);
+      },
+      error: function(error){ //incase we didn't get /songs log error
+      console.log('error', error);
+    }
+  });
+}
+
+$('#addSongButton').on('click', function(){ //listening for click on addSongButton storing values of input for title and artist
+  var newSongTitle = $('#title').val(); // defining newSongObject with user input
+  var newSongArtist = $('#artist').val(); // defining newSongObject with user input
+  var newSongObject = {
+    title: newSongTitle,
+    artist: newSongArtist
+  };
+  console.log(newSongObject);
+  $.ajax({
+    type: 'POST', // now posting
+    url: '/newSong', // web location localhost:3000/newSong
+    data: newSongObject, //data becomes req.body in app.js because of body parser
+    success: function(response){
+      console.log('response', response);
+      getAllSongs(); // to run each time new post is made
+    },
+    error: function(error){
+      console.log('error', error);
+    }
+  });
+});
+
+});
